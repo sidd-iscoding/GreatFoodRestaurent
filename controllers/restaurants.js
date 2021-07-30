801ade5d5654630ed08d27733d78294b822b2a12
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-07-31 01:47:28 +0530

    try catch block for all end points with DB

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -3,23 +3,38 @@ const Restaurant = require('../Models/Restaurant');
 //@desc   get all restaurents
 //@routes GET api/v1/restaurents
 //@access No Signin required
-exports.getrestaurants=(req,res,next)=>{
-    res.status(404).json({success:true,msg:"show all restaurents"});
+exports.getrestaurants=async(req,res,next)=>{
+    try {
+        const restaurant=await Restaurant.find(); 
+        res.status(200).json({success:true,msg:"show all restaurents",count:restaurant.length,data:restaurant});
+    } catch (error) {
+        res.status(400).json({success:false,msg:"can't create new restaurents!"});
+    }
+    
 }
 
 //@desc   get a restaurent
 //@routes GET api/v1/restaurent/:id
 //@access No Signin required
-exports.getrestaurant=(req,res,next)=>{
-    res.status(404).json({success:true,msg:`Get restaurent ${req.params.id}`});
+exports.getrestaurant=async (req,res,next)=>{
+    try {
+        const restaurant=await Restaurant.findById(req.params.id); 
+        if(!restaurant){
+            return res.status(400).json({success:false,msg:"id not correctly formatted"});    
+        }
+        res.status(200).json({success:true,msg:"show the restaurent",data:restaurant});
+    } catch (error) {
+        res.status(400).json({success:false,msg:`can't Get restaurent ${req.params.id}`});
+    }
+    
 }
 //@desc   create new restaurents
 //@routes POST api/v1/restaurents
 //@access Signin required
 exports.createrestaurant=async (req,res,next)=>{
     try {
-        const restaurent=await Restaurant.create(req.body);
-        res.status(201).json({success:true,msg:"create new restaurents",data: restaurent});
+        const restaurant=await Restaurant.create(req.body);
+        res.status(201).json({success:true,msg:"create new restaurents",data: restaurant});
     } catch (error) {
         res.status(400).json({success:false,msg:"can't create new restaurents!"});
     }
@@ -29,13 +44,33 @@ exports.createrestaurant=async (req,res,next)=>{
 //@desc   update restaurents
 //@routes PUT api/v1/restaurents/:id
 //@access  Signin required
-exports.updaterestaurant=(req,res,next)=>{
-    res.status(404).json({success:true,msg:`Update restaurent ${req.params.id}`});
+exports.updaterestaurant=async(req,res,next)=>{
+    try {
+        const restaurant=await Restaurant.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true  
+        });
+        if(!restaurant){
+            return res.status(400).json({success:false,msg:"id not correctly formatted"});    
+        }
+        res.status(200).json({success:true,msg:"show the updated restaurent",data:restaurant});
+    } catch (error) {
+        res.status(400).json({success:false,msg:`can't update restaurent with ${req.params.id}`});
+    }
 }
 
 //@desc   delete restaurent
 //@routes DELETE api/v1/restaurents/:id
 //@access Signin required
-exports.deleterestaurant=(req,res,next)=>{
-    res.status(404).json({success:true,msg:`Delete restaurent ${req.params.id}`});
+exports.deleterestaurant=async(req,res,next)=>{
+    try {
+        const restaurant=await Restaurant.findByIdAndDelete(req.params.id)
+        if(!restaurant){
+            return res.status(400).json({success:false,msg:"id not correctly formatted"});    
+        }
+        res.status(200).json({success:true,msg:" restaurent deleted"});
+    } catch (error) {
+        res.status(400).json({success:false,msg:`can't Get restaurent ${req.params.id}`});
+    }
+    
 }
