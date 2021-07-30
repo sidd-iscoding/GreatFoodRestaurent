533779ba1ed14d81d6908e97798cbebb87b462ce
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-07-30 10:03:07 +0530

    Addition of middleware router

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const restaurents = require('./routes/routes');
+
 //lOAD ENV VARS
 dotenv.config({path:'./config/config.env'});
 app=express();
 const PORT=process.env.PORT ;
 const ENV=process.env.NODE_ENV;
+
+//Mount the router(restaurents) on a specific url(/api/v1/restaurents)
+app.use('/api/v1/restaurents',restaurents);
+
+
+
 app.listen(PORT,console.log(`server is running in ${ENV} mode on port ${PORT}`));
