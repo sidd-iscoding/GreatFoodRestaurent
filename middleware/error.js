8a4fce1de40ca06a163c095c3ebf82d34f3afa89
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-08-01 14:17:11 +0530

    error from stacktrace is handled in error.js

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,20 @@
+const ErrorResponse = require("../utility/ErrorResponse");
+
 const errorHandler= (err,req,res,next) => {
+    let error={...err}; //use of spread operator
+    error.message=err.message;
     //log to console for dev
    console.log(err.stack);
-    res.status(err.statusCode || 500).json({
+
+   //Mongoose Bad ObjectId
+   if(err.name === 'CastError'){
+        const message=`Requested Resource not found with id ${err.value} `;
+        console.log(err.name);
+        error=new ErrorResponse(message, 404);
+   }
+    res.status(error.statusCode || 500).json({
         success:false,
-        error:err.message || 'Server Error!'
+        error:error.message || 'Server Error!'
     });
 };
 
